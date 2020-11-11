b6d24465879ed0add5b3ee5839391e1688bfb3fc
logicfirst <slee@logicfirstllc.com>
2020-11-11 14:18:28 -0500

    commit tree 5c0d89aade9b2781d9256f33c97aa4819fbdfbee

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,15 +1,17 @@
+require 'pry'
+
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  array.count do
-    |element|
-    element.class == String
+  string_count = []
+  array.count do |element|
+    if element.class == String
+      string_count << element
+    end
   end
+  string_count.count
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  array.count do
-    |element|
-    element == ""
-  end
+  array.count("")
 end
